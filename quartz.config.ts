c9d505c03512b1ebb26e5c146a968853117a612b
Mateo <mattguzsal@gmail.com>
2024-05-29 16:06:29 -0500

    Quartz sync: May 29, 2024, 4:06 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Daniel",
+    pageTitle: "ManageEngine",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
